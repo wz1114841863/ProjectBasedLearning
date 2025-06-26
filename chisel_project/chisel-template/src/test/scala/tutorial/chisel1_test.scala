59ccd26c593b14a77e7b34142e3348afa29e87ee
wz1114841863 <18801383735@163.com>
2025-06-26 11:35:48 +0800

    modify scala

--- chisel_project/chisel-template/src/test/scala/tutorial/chisel1_test.scala
+++ chisel_project/chisel-template/src/test/scala/tutorial/chisel1_test.scala
@@ -6,23 +6,39 @@ import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
 
 class PassthroughSpec extends AnyFreeSpec with Matchers with ChiselSim {
-  "Passthrough should forward input" in {
-    // 创建被测模块实例
-    simulate(new Passthrough()) { dut =>
-      // 测试值序列
-      val testValues = Seq(0, 1, 15)
-
-      // 遍历所有测试值
-      testValues.foreach { value =>
-        // 驱动输入信号
-        dut.io.in.poke(value.U)
-
-        // 立即验证输出(组合逻辑)
-        dut.io.out.expect(value.U)
-
-        // 可选:打印调试信息
-        println(f"Input = $value%3d, Output = ${dut.io.out.peek().litValue}%3d")
-      }
+    "Passthrough should forward input" in {
+        // simulate有两个输入, 一个是模块实例化,
+        // 一个是参数为dut的函数, 这里采用了匿名函数和柯里化
+        simulate(new Passthrough()) { dut =>
+            val testValues = Seq(0, 1, 15)
+            testValues.foreach { value =>
+                dut.io.in.poke(value.U)
+                dut.io.out.expect(value.U)
+                println(
+                  f"Input = $value%3d, Output = ${dut.io.out.peek().litValue}%3d"
+                )
+            }
+        }
     }
-  }
+}
+
+class PassthroughGenerator extends AnyFreeSpec with Matchers with ChiselSim {
+    "PassthroughGenerator should forward input" in {
+        // 测试不同宽度的生成器
+        Seq(8, 16).foreach { width =>
+            simulate(new PassthoughGenerator(width)) { dut =>
+                val testValues = Seq(0, 1, (1 << width) - 1)
+
+                testValues.foreach { value =>
+                    dut.io.in.poke(value.U(width.W))
+                    dut.io.out.expect(value.U(width.W))
+
+                    println(
+                      f"Width = $width, Input = $value%3d, Output = ${dut.io.out.peek().litValue}%3d"
+                    )
+                }
+            }
+        }
+        println("Success!")
+    }
 }
